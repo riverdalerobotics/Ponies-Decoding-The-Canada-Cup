4fff2f477b6e34e899dc6f47b95bde3ad82fa061
FrankieZO <fzoliveira@icloud.com>
2026-04-04 16:56:44 -0400

    added to teleop v1 + autopoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TeleopV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TeleopV1.java
@@ -3,11 +3,18 @@ package org.firstinspires.ftc.teamcode.TestCode;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.CommandScheduler;
+import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
+import com.seattlesolvers.solverslib.command.ParallelDeadlineGroup;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import com.seattlesolvers.solverslib.command.button.Button;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 
+import org.firstinspires.ftc.teamcode.Commands.ChassisCommands.ChassisLookToAprilTagTeleOp;
 import org.firstinspires.ftc.teamcode.Commands.ChassisCommands.FieldDefaultCommand;
 import org.firstinspires.ftc.teamcode.Commands.IntakeCommands.IntakeDefaultCommand;
+import org.firstinspires.ftc.teamcode.Commands.ShooterCommands.FeedShooter;
+import org.firstinspires.ftc.teamcode.Commands.ShooterCommands.RevThreeToVelo;
 import org.firstinspires.ftc.teamcode.Commands.ShooterCommands.ShooterDefaultCommand;
 import org.firstinspires.ftc.teamcode.RobotConstants;
 import org.firstinspires.ftc.teamcode.Subsystems.ChassisSubsystem;
@@ -26,7 +33,7 @@ public class TeleopV1 extends CommandOpMode {
 
     GamepadEx driver = new GamepadEx(gamepad1);
     TelemetryManager telemetryM;
-    Button shoot, rev;
+    Button shoot, rev, lockOn;
 
     @Override
     public void initialize() {
@@ -53,5 +60,28 @@ public class TeleopV1 extends CommandOpMode {
         intakeDefault = new IntakeDefaultCommand(intake);
         intake.setDefaultCommand(intakeDefault);
 
+    }
+
+    @Override
+    public void run(){
+        CommandScheduler.getInstance().run();
+
+        schedule(chassisDefault);
+
+        rev.whileHeld(
+                new RevThreeToVelo(snap, crackle, pop, limelight)
+        );
+
+        shoot.whenPressed(
+                new SequentialCommandGroup(
+                        new FeedShooter(snap),
+                        new FeedShooter(crackle),
+                        new FeedShooter(pop) //need timer
+                )
+        );
+
+        lockOn.whileHeld(
+                new ChassisLookToAprilTagTeleOp(chassis, limelight, telemetryM, driver)
+        );
     }
 }
